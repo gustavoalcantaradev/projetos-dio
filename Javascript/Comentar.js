f17287e143ddd98a47dc757b8d47f566d9d10b0a
Gustavo Alcantara de Jesus <107977597+gustavoalcantaradev@users.noreply.github.com>
2022-07-11 01:35:06 -0300

    primeiras aulas

---
+++ Javascript/Comentar.js
@@ -0,0 +1,9 @@
+// var y = 9  comenta somente uma linha
+
+
+
+/* var y = 9
+var b = 5
+var g = 5   comenta um grupo */ 
+
+
---
+++ Javascript/Funções.js
@@ -0,0 +1,5 @@
+function soma(a, b) {   // função/parametros que eu quero
+    return a + b; // resultado = 8
+}
+
+soma (3 , 5);  // o que será utilizado
